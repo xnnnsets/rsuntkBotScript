05d184952a9d01600d7edcebd4e366603d13392c
rsuntk <rissu.ntk@gmail.com>
2025-01-06 19:59:06 +0700

    Initial
    Signed-off-by: rsuntk <rissu.ntk@gmail.com>

---
+++ rsuntkbot.sh
@@ -0,0 +1,13 @@
+#!/bin/env bash
+msg="*$TITLE*
+\`\`\`
+$2
+\`\`\`
+*Note: Always backup working boot before flash\\.*
+"
+file="$1"
+curl -s -F document=@$file "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
+	-F chat_id="$CHAT_ID" \
+	-F "disable_web_page_preview=true" \
+	-F "parse_mode=markdownv2" \
+	-F caption="$msg"

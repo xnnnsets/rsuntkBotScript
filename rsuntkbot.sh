006da3ee5c2148ef15b4cda9c19a6fef80c7031e
HiN <167276632+xnnnsets@users.noreply.github.com>
2025-03-18 01:56:15 +0700

    Update rsuntkbot.sh

--- rsuntkbot.sh
+++ rsuntkbot.sh
@@ -5,11 +5,8 @@ msg="*$TITLE*
 $2
 \`\`\`
 *Note: Always backup working boot before flash\\.*
-Thanks for Source [rsuntk](https://github.com/rsuntk) [rsuntk-open-source](https://github.com/rsuntkOrgs)\\.
+*Thanks for Source builder and kernel rsuntk, rsuntkOrgs)\\.*
 "
-
-# Escape any special markdown characters
-msg=$(echo "$msg" | sed 's/[\*_\[\]()~`>#+-=|{}.!]/\\&/g')
 
 file="$1"
 

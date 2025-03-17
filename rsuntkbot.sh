76220b6700ae474ba80f99403f08a383075427b3
HiN <167276632+xnnnsets@users.noreply.github.com>
2025-03-18 00:03:35 +0700

    Update rsuntkbot.sh

--- rsuntkbot.sh
+++ rsuntkbot.sh
@@ -11,5 +11,5 @@ file="$1"
 curl -s -F document=@$file "https://api.telegram.org/bot$BOT_TOKEN/sendDocument" \
 	-F chat_id="$CHAT_ID" \
 	-F "disable_web_page_preview=true" \
-	-F "parse_mode=MarkdownV2" \
+	-F "parse_mode=markdownv2" \
 	-F caption="$msg"
